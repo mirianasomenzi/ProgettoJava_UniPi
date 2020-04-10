ae62c551590585ff00595c57ff8f5f2e504bf21a
utente <utente@DESKTOP-UDB2ARS>
2020-04-10 10:53:11 +0200

    DateFormat e il metodo setLenient e data.parse + eccezioni

--- src/progettojava/GestoreLocale.java
+++ src/progettojava/GestoreLocale.java
@@ -1,7 +1,11 @@
 package progettojava;
 import java.util.Scanner;
 import java.util.Vector;
-import java.util.Calendar.Builder;
+import java.util.Date;
+//import java.util.Calendar.Builder;
+import java.text.DateFormat;//aggiunto
+import java.lang.IllegalArgumentException;
+import java.text.ParseException;//aggiunto
 import progettojava.Affitto.Affitto;
 import progettojava.Affitto.Catering;
 import progettojava.Affitto.CateringAnimazione;
@@ -11,16 +15,33 @@ public class GestoreLocale {
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		Vector <Integer> calendario = new Vector<Integer> ();
-		Builder builder = new Calendar.Builder().setCalendarType("iso8601").build();
+		//Calendar builder = new Calendar.Builder().setCalendarType("iso8601").build();
 		//public Calendar.Builder setDate(int anno, int mese, int giorno);
 		CateringAnimazione nuovocateringanimazione = new CateringAnimazione();
 		Affitto nuovoaffitto = new Affitto();
 		Catering nuovocatering = new Catering();
 		
-		System.out.println("In che giorno vuoi organizzare il compleanno? (inserisci data AA/MM/GG) ");
+		System.out.println("In che giorno vuoi organizzare il compleanno? (inserisci data gg/mm/yy) ");
 		
-	
+		String StringaData = input.nextLine();
+		//converto la stringa in un oggetto della classe Date
+		//tutto questo ci conviene metterlo in un metodo ausiliario?
+		//oppure in affitto che modifichiamo in Prenotazione e affitto dal momento che ogni prenotazione prevede un affitto?
+		try {
+			DateFormat data = DateFormat.getDateInstance(DateFormat.SHORT);
+			// questo metodo permette di fare controlli sulla stringa inserita prima di convertirla
+			data.setLenient(false);
+			
+			Date d = data.parse(StringaData);
+			
+		} 
+		catch (IllegalArgumentException e) { //non so se sia utile questa eccezione e come gestirle meglio
+		} 
+		catch (ParseException  e) {
+			System.out.println ("Formato data non valido");
 		}
+		
+	}
 
 }
 
