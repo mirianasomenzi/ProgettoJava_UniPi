83c5bce4fe628dc2720dc52c33847bd721ab1afc
Miriana <Miriana@NB-Miriana.lan>
2020-04-10 12:30:50 +0200

    Ho creato il menu con le azioni di poter effettuare secondo la consegna del prof e ho staccato Gestore Prenotazioni creando una nuova classe in cui gestire la funzione di aggiunta prenotazioni

--- src/progettojava/GestoreLocale.java
+++ src/progettojava/GestoreLocale.java
@@ -2,10 +2,11 @@ package progettojava;
 import java.util.Scanner;
 import java.util.Vector;
 import java.util.Date;
-//import java.util.Calendar.Builder;
-import java.text.DateFormat;//aggiunto
-import java.lang.IllegalArgumentException;
-import java.text.ParseException;//aggiunto
+import java.util.Locale;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import progettojava.Affitto.Affitto;
 import progettojava.Affitto.Catering;
 import progettojava.Affitto.CateringAnimazione;
@@ -14,41 +15,42 @@ public class GestoreLocale {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
-		Vector <Integer> calendario = new Vector<Integer> ();
-		//Calendar builder = new Calendar.Builder().setCalendarType("iso8601").build();
-		//public Calendar.Builder setDate(int anno, int mese, int giorno);
-		CateringAnimazione nuovocateringanimazione = new CateringAnimazione();
-		Affitto nuovoaffitto = new Affitto();
-		Catering nuovocatering = new Catering();
-		
-		System.out.println("In che giorno vuoi organizzare il compleanno? (inserisci data gg/mm/yy) ");
-		
-		String StringaData = input.nextLine();
-		//converto la stringa in un oggetto della classe Date
-		//tutto questo ci conviene metterlo in un metodo ausiliario?
-		//oppure in affitto che modifichiamo in Prenotazione e affitto dal momento che ogni prenotazione prevede un affitto?
-		try {
-			DateFormat data = DateFormat.getDateInstance(DateFormat.SHORT);
-			// questo metodo permette di fare controlli sulla stringa inserita prima di convertirla
-			data.setLenient(false);
-			
-			Date d = data.parse(StringaData);
-			
-		} 
-		catch (IllegalArgumentException e) { //non so se sia utile questa eccezione e come gestirle meglio
-		} 
-		catch (ParseException  e) {
-			System.out.println ("Formato data non valido");
+		GestorePrenotazioni gestorePrenotazioni = new GestorePrenotazioni (); // creato oggetto gestorePrenotazioni
+		stampaMenu(); // stampare il menu 
+		int scelta = input.nextInt(); //  acquisisco input
+		while (scelta !=8) {
+			switch (scelta) {
+			case 1:
+				gestorePrenotazioni.aggiungiPrenotazione(); //richiamo il metodo aggiungiPrenotazione che si trova in GestorePrenotazioni
+				break;
+			//case 2:
+			//case 3:
+			//case 4:
+			//case 5:
+			//case 6:
+			//case 7:
+			//case 8: commentati perchè non ancora implementate queste funzioni
+			default:
+				System.out.println("Funzione non ancora implementata");
+				// default da togliere quando avremo implementato tutte le funzioni
+			}
+		stampaMenu();
+		scelta = input.nextInt(); // prima di tornare nel while mi richiede cosa voglio fare stampando di nuovo il menu
 		}
+	}
+	 // creazione menu del programma  
+	// \n serve per far andare a capo ( new line)
+	public static void stampaMenu() {
+		System.out.println("Menu programma:\n"
+				+ "1 = aggiungere nuove prenotazioni \n"
+				+ "2 = visualizzare elenco prenotazioni in ordine di data \n"
+				+ "3 = visualizzare prenotazioni che prevedono catering \n"
+				+ "4 = visualizzare prenotazioni che prevedono animazione \n"
+				+ "5 = visualizzare prima data disponibile a partire dal giorno corrente \n"
+				+ "6 = visualizzare prenotazioni effettuate da cliente selezionato \n"
+				+ "7 = eliminare prenotazioni \n"
+				+ "8 = esci \n");
+		System.out.println("Inserisci numero : ");
 		
 	}
-
 }
-
-// Calendar.Builder   setDate(int year, int month, int dayOfMonth) 
-//vector per memorizzare le date inserite dall'utente perchè non possiamo sapere
-//in anticipo quante saranno, in modo da poter poi verificare se una data è libera o no
-// n1 = chiedere all'utente di inserire i dati per verificare la disponibilità, se data disponibile passa a affitto se
-// non disponibile scegli altra data 
-// if v. isEmpty())
-// 
---
+++ src/progettojava/GestorePrenotazioni.java
@@ -0,0 +1,59 @@
+package progettojava;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Vector;
+
+import progettojava.Affitto.Affitto;
+import progettojava.Affitto.Catering;
+import progettojava.Affitto.CateringAnimazione;
+
+public class GestorePrenotazioni { // creato una nuova classe dove poter gestire le prenotazioni (quindi le richieste che il prof fa
+	                               // nella consegna es. aggiungere prenotazione, eliminarla, visualizzare ecc) perchè facendo 
+	                               // riferimento a metodi non statici non potevano stare nel main che è static 
+	
+	private Vector <Date> calendario = new Vector<Date> ();// vettore di date, cambiato di posizione perchè in questo modo è accessibile da
+	                                                       // tutti i metodi e non viene sovrascritto ogni volta, e private così è
+	                                                       // modificabile solo dalla classe GestorePrenotazioni
+	public GestorePrenotazioni() {
+		
+	}
+	   public void aggiungiPrenotazione () { // non static perchè fa riferimento a metodi non statici es. il calendario che cambia
+			Scanner input = new Scanner (System.in);	    
+			CateringAnimazione nuovocateringanimazione = new CateringAnimazione();
+			Affitto nuovoaffitto = new Affitto();
+			Catering nuovocatering = new Catering();
+			Locale.setDefault(Locale.ITALIAN); // settare data in italiano 
+			System.out.println("In che giorno vuoi organizzare il compleanno? (inserisci data gg/mm/yy) ");
+
+			String stringaData = input.nextLine();
+			//converto la stringa in un oggetto della classe Date
+			//tutto questo ci conviene metterlo in un metodo ausiliario?
+			//oppure in affitto che modifichiamo in Prenotazione e affitto dal momento che ogni prenotazione prevede un affitto?
+			try {
+				
+				DateFormat data = DateFormat.getDateInstance(DateFormat.SHORT);
+				// questo metodo permette di fare controlli sulla stringa inserita prima di convertirla
+				data.setLenient(false);
+				
+				Date d = data.parse(stringaData);
+			    
+			    if (!calendario.contains(d)) { // se il calendario non contiene la data selezionata
+			    	System.out.println("Data disponibile");
+			    	calendario.add(d);
+			    	 if (calendario.contains(d)) {
+					    	System.out.println("Data prenotazione confermata!"); 
+			    	 }	    	 
+			    } 			    
+			    else  
+			    	System.out.println ("Data occupata");   
+			}     
+		
+			catch (ParseException  e) {
+				System.out.println ("Formato data non valido");
+			}
+		}
+}
